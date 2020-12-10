806ea7641ea751da82955b2a8f61f97862be92ca
Vincent OLeary <vjoleary@users.noreply.github.com>
2020-12-10 18:00:32 -0500

    first 3 exercises for conditionals

---
+++ script.js
@@ -0,0 +1,42 @@
+if(1+1 > 5){
+    console.log("True fact");
+} else{
+    console.log("FAKE NEWS")
+}
+
+if(4*5 <= 20){
+    console.log("True fact");
+} else{
+    console.log("FAKE NEWS")
+}
+
+if(6-2 >= 0){
+    console.log("True fact");
+} else{
+    console.log("FAKE NEWS")
+}
+
+let likesDogs = true
+if(letDogs = true){
+    console.log("You're a dog person");
+}else console.log("You must serve cats")
+
+let birthYear = 1995
+if(birthYear <= 1921){
+    console.log("You're a dead man walking")
+} else if(birthYear <= 1927){
+    console.log("You fought in WW2")
+} else if(birthYear <= 1945){
+    console.log("You love watching the History Channel")
+} else if(birthYear <= 1965){
+    console.log("You're a member of Congress")
+} else if(birthYear <= 1976){
+    console.log("You've tried drugs")
+} else if(birthYear <= 1994){
+    console.log("You've been asked to fix a computer")
+} else{
+    console.log("You're viral on TikTok")
+}
+
+
+    
