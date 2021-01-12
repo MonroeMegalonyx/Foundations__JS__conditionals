4a9bcd68a25b3a0337904746ca70ea51afb61805
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-12 11:08:17 -0500

    Rebuilding local workspace on backup computer

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-//1. Check Your Math
-//Use JavaScript to check and see if the following comparisons are true. For each of the following, log `True fact!` to the console if the condition evaluates to true and `LIES!` to the console if it doesn't.
+// LIGHTNING EXERCISE
+// Bon Voyage!
+// In your JavaScript file, declare a new variable called likesToTravel and set it equal to true or false.
+// If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.
 
-//Is the sum of 1 and 1 greater than 5?
+let likesToTravel = false
+if(likesToTravel === true){
+    console.log("Bon Voyage")
+} else{
+    console.log("Enjoy your couch")
+}
+
+// LIGHTNING EXERCISE 2
+
+let favoriteFood = "grilled cheese"
+if(favoriteFood === "fried pickles"){
+    console.log("Congratulations! You have excellent taste!")
+} else{
+    console.log("Clearly you have not tried fried pickles or pepperoni rolls.")
+}
+
+// PRACTICE
+// 1. Check Your Math
+// Use JavaScript to check and see if the following comparisons are true. For each of the following, log `True fact!` to the console if the condition evaluates to true and `LIES!` to the console if it doesn't.
+
+// Is the sum of 1 and 1 greater than 5?
 if(1+1 > 5){
     console.log(`True fact`);
 } else{
     console.log(`FAKE NEWS`)
 }
 
-//Is the product of 4 and 5 less than our equal to 20?
+// Is the product of 4 and 5 less than our equal to 20?
 if(4*5 <= 20){
     console.log(`True fact`);
 } else{
     console.log(`FAKE NEWS`)
 }
 
-//Is the difference between 6 and 2 greater than or equal to 0?
+// Is the difference between 6 and 2 greater than or equal to 0?
 if(6-2 >= 0){
     console.log(`True fact`);
 } else{
     console.log(`FAKE NEWS`)
 }
 
-//2. Dog People vs. Cat People
-//In your JavaScript file, declare a new variable called likesDogs and set it equal to true or false.
-//If likesDogs is true, log `You're a dog person!` to the console. If not, log `You're a cat person!` to the console.
+// 2. Dog People vs. Cat People
+// In your JavaScript file, declare a new variable called likesDogs and set it equal to true or false.
+// If likesDogs is true, log `You're a dog person!` to the console. If not, log `You're a cat person!` to the console.
+
 let likesDogs = true
 if(letDogs = true){
     console.log(`You're a dog person`);
 }else console.log(`You must serve cats`)
 
-//3. Generation Gaps
-//Declare a variable called birthYear and set it equal to your birth year.
-//Write a conditional statement that checks to see which generation you're part of and log a sentence about that generation to the console.
-//Try changing the value of birthYear and see what happens.
+// 3. Generation Gaps
+// Declare a variable called birthYear and set it equal to your birth year.
+// Write a conditional statement that checks to see which generation you're part of and log a sentence about that generation to the console.
+// Try changing the value of birthYear and see what happens.
+
 let birthYear = 1995
 if(birthYear <= 1921){
     console.log(`You're a dead man walking`)
@@ -51,12 +75,13 @@ if(birthYear <= 1921){
     console.log(`You're viral on TikTok`)
 }
 
-//4. Greetings
-//In your JavaScript file, create a new object representing a person. Your object should have at least two properties: one for the person's name and one for their preferred language.
-//Write a conditional that checks for the following:
-//If the person's preferred language is English, print `Hello, [name]!` to the console.
-//If the person's preferred language is Spanish print `Hola, [name]!` to the console.
-//If their preferred language is French, print `Bonjour, [name]!` to the console.
+// 4. Greetings
+// In your JavaScript file, create a new object representing a person. Your object should have at least two properties: one for the person's name and one for their preferred language.
+// Write a conditional that checks for the following:
+// If the person's preferred language is English, print `Hello, [name]!` to the console.
+// If the person's preferred language is Spanish print `Hola, [name]!` to the console.
+// If their preferred language is French, print `Bonjour, [name]!` to the console.
+
 let definitelyRealHuman = {
     name: `Barry`,
     language: `English`
@@ -70,5 +95,8 @@ if(definitelyRealHuman.language === `English`){
     console.log(`Bonjour ${definitelyRealHuman.name}!`)
 }
 
-//5. Grandma's Kitchen
+// 5. Grandma's Kitchen
 
+// 6. Student Grades
+
+// CHALLENGE
